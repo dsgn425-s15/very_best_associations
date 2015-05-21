ff986ce4f425fc864d987031a422159dc4e60982
daufford <david.ufford@gmail.com>
2015-05-21 13:33:10 -0500

    finished assignment

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,6 @@
 class Neighborhood < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => { :scope => :city }
+  validates :city, :presence => true
+  has_many :venues
+  has_many :favorites, :through => :venues
 end
