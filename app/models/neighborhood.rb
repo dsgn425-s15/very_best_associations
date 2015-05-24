00ee1fdf908c4f1f0f9fc39a442ff942c48fec1d
nicoleroden <nicole.roden@gmail.com>
2015-05-24 15:37:49 -0500

    users and faves by hood

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,5 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => {:scope => :city}
   #validates :city, :presence => true, :uniqueness => true
   has_many :venues
+  has_many :favorites, :through => :venues
 end
