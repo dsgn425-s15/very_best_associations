29ae638218b90916b7634141906ebcf5387a4e32
nbrowski <nbrower@dhcp-10-105-17-82.wireless.northwestern.private>
2015-05-27 11:04:36 -0500

    step 4 complete

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,6 @@
 class Dish < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => {:scope => :cuisine}
   validates :cuisine_id, :presence => true
-  belongs_to :cuisines #is plural here allowed if I've allowed the same dish name to belong to multiple cuisines?
+  belongs_to :cuisine #is plural here allowed if I've allowed the same dish name to belong to multiple cuisines?
   has_many :favorites
 end
