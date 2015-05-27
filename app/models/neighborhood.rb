f884cf08967d2e9b7a44709cad42d9d37c7cc72e
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-05-26 22:30:53 -0500

    Parts 1-2

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,10 @@
 class Neighborhood < ActiveRecord::Base
+
+validates :name, :presence => true, :uniqueness => true
+
+validates :name, :uniqueness => { :scope => :city }
+
+
+has_many :venues
+
 end
