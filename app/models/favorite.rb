261c762df0e0189d7c772cf3b6b440f4a7b4365b
jptucker <john.pelton.tucker@gmail.com>
2015-05-27 10:42:46 -0500

    Started homework 8

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,6 @@
 class Favorite < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :venue
+  belongs_to :dish
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,7 @@
 class Venue < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :address, :presence => true
+
+  belongs_to :neighborhood
+  has_many :favorites
 end
