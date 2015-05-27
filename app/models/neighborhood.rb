5ce18ac527919855cf42fac28e374db380c07aab
kjplank <plank@KJs-MacBook-Air.local>
2015-05-26 22:30:35 -0500

    Completed Parts 1-4

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,5 @@
 class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :name, :presence => true, :uniqueness => { :name => :city }
+  has_many :venues
 end
